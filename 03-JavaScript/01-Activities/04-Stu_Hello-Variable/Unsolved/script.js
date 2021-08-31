7f9b251f86e8c10ddf472c252c8da2447a313c0a
ETWilliams10 <williamsevan10@gmail.com>
2021-08-30 22:01:24 -0400

    week 3 day 1 activity work

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,17 @@
 // Write Your JavaScript Code Here
+
+var personName = "Evan";
+var pets = 0;
+var funFact = "I went to the hospital for sticking an uncooked sphagetti noodle in my ear.";
+
+console.log("My name is " + personName);
+console.log("I have " + pets + " pets(s)");
+console.log("Fun fact: " + funFact);
+
+personName = "I don't have any friends.";
+pets = "0 obviously";
+funFact = "I'm lonely.";
+console.log("My name is " + personName);
+console.log("I have " + pets + " pets(s)");
+console.log("Fun fact: " + funFact);
+
